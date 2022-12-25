daf5c29bd82407e740f0155fbff91139784990d9
iPaoloTM <paoloalipro@gmail.com>
2022-12-25 17:51:12 +0100

    weather forecast GET

--- processes/pointSearch/controllers/weatherController.js
+++ processes/pointSearch/controllers/weatherController.js
@@ -26,3 +26,28 @@ module.exports.current = async (req, res) => {
         message: 'current weather in '+req.query.city+' is '+desc
     });
 };
+
+module.exports.forecast = async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    const city=req.query.city;
+    const response = await fetch("https://api.weatherbit.io/v2.0/forecast/daily?city="+city+"&key=ff2dde4ae1b54a0582d580d568cda284&lang=it");
+    const data = await response.json();
+    const array = [];
+    var forecasts = "In the next 16 days, in "+city+" the weather will be:"
+
+    data.data.forEach((item, i) => {
+      array[i]=item.weather["description"];
+      forecasts+=" on day "+i+", "+array[i]+'\n'
+    });
+
+    res.status(200).json({
+        success: true,
+        user: req.loggedUser,
+        message: forecasts,
+        array: array
+    });
+};
--- processes/pointSearch/routes/apiV1/weather.js
+++ processes/pointSearch/routes/apiV1/weather.js
@@ -8,6 +8,8 @@ const eventHandler = require('../../controllers/weatherController');
 
 router.get('/current', eventHandler.current);
 
+router.get('/forecast', eventHandler.forecast);
+
 router.get('/about', function(req, res) {
   res.send('About weather');
 });
