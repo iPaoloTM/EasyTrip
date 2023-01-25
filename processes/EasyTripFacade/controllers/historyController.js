ce394d5dd70730c77c21ab5e2de0176ad201814c
Simone <simone.compri@gmail.com>
2023-01-25 17:43:14 +0100

    Base /history endpoints

--- processes/EasyTripFacade/controllers/historyController.js
+++ processes/EasyTripFacade/controllers/historyController.js
@@ -5,8 +5,38 @@ let MSG = {
     serverError: "Server error", //Error code: 500
 }
 
-module.exports.saveTrip = async (req, res) => {}
+module.exports.saveTrip = async (req, res) => {
 
-module.exports.getTrips = async (req, res) => {}
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
 
-module.exports.deleteTrip = async (req, res) => {}
+    res.status(200).json({
+        user: req.loggedUser
+    });
+}
+
+module.exports.getTrips = async (req, res) => {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    
+    res.status(200).json({
+        user: req.loggedUser
+    });
+}
+
+module.exports.deleteTrip = async (req, res) => {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    
+    res.status(200).json({
+        user: req.loggedUser
+    });
+}
