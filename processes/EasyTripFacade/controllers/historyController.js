008a11c883ba06e3d677851a0759991df3fdefbd
Simone <simone.compri@gmail.com>
2023-01-27 21:47:38 +0100

    History edits
    Now:
    - New POST /history API
    - GET /history build url
    - DELETE /history/:id refers to _id

--- processes/EasyTripFacade/controllers/historyController.js
+++ processes/EasyTripFacade/controllers/historyController.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const model = require('../models/users');
 const User = (mongoose.models && mongoose.models.User) ? mongoose.models.User : mongoose.model('User',model);
 
-const { INTERESTS, PROFILES, POINT_SEARCH_URL, PATH_SEARCH_URL } = require('../../../common/dataStructures');
+const { INTERESTS, PROFILES } = require('../../../common/dataStructures');
 const { cleanWrtStruct, arrayToStr } = require('../../../common/functions');
 
 let MSG = {
@@ -21,8 +21,8 @@ module.exports.saveTrip = async (req, res) => {
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
 
-        const obj = JSON.parse(req.body);
-        const urlString = obj.url;
+        const tripInfo = req.body;
+        let isSafe,actualParams;
 
         User.findById(req.loggedUser.user_id, (err, user) => {
           if (err) {
@@ -31,49 +31,106 @@ module.exports.saveTrip = async (req, res) => {
                   error: MSG.serverError
               });
           } else {
-
-            const parsedUrl = url.parse(urlString);
-
-            const pathname = parsedUrl.pathname;
-
-            const type = pathname.split('/')[3];
-
-            const params = new URLSearchParams(parsedUrl.search);
-
-            const isSafe = true;
-
-            if (type == 'travel') {
-
-              if (params.weather != "true" || params.weather != "false" || params.bikes != "true" || params.bikes != "false" || interests = cleanWrtStruct(interests,INTERESTS)).length == 0) {
-                isSafe = false;
-              }
-
-            } else if (type == 'destination') {
-              if (params.weather != "true" || params.weather != "false" || params.bikes != "true" || params.bikes != "false" || interests = cleanWrtStruct(interests,INTERESTS)).length == 0 || params.limit < 0 || params.limit > 8 || typeof params.minDistance != "number" || typeof params.maxDetour != "number") {
-                isSafe = false;
-              }
-            }
+            isSafe = tripInfo.route != undefined && tripInfo.type != undefined && tripInfo.parameters != undefined;
+            actualParams = new Set(["interest"]);
 
             if (isSafe) {
-
-            const date = new Date().toISOString();
-
-            const id = uuid.v4();
-
-            const historyObject = JSON.stringify(Object.fromEntries('{"id":"'+id+'", "dateTime": "'+date+'", "type": "'+type+'", "parameters": "'+params+'", "url": "'+url+'"}'));
-            console.log(historyObject);
-
-            user.history.push(historyObject);
-            user.save((err, updatedUser) => {
-                if(err) {
-                    res.status(500).send(err);
-                } else {
-                    res.status(200).json(updatedUser);
+                tripInfo.parameters.interest = tripInfo.parameters.interest != undefined
+                                                ? (Array.isArray(tripInfo.parameters.interest)
+                                                    ? tripInfo.parameters.interest
+                                                    : [tripInfo.parameters.interest])
+                                                : [];
+                
+                if (tripInfo.parameters.weather != undefined) {
+                    if (typeof tripInfo.parameters.weather == "boolean") {
+                        actualParams.add("weather");
+                    } else {
+                        delete tripInfo.parameters.weather;
+                    }
                 }
-            }); } else {
-              res.status(500).json({
-                  error: MSG.serverError
-              });
+                if (tripInfo.parameters.bikes != undefined) {
+                    if (typeof tripInfo.parameters.bikes == "boolean") {
+                        actualParams.add("bikes");
+                    } else {
+                        delete tripInfo.parameters.bikes;
+                    }
+                }
+                if (tripInfo.parameters.interest.length) {
+                    if (!(tripInfo.parameters.interest = cleanWrtStruct(tripInfo.parameters.interest,INTERESTS)).length) {
+                        tripInfo.parameters.interest = [];
+                    }
+                }
+                switch (tripInfo.type) {
+                    case "destination":
+                        isSafe = tripInfo.parameters.address != undefined;
+                        if (isSafe) actualParams.add("address");
+                        break;
+                    case "travel":
+                        isSafe = tripInfo.parameters.start != undefined && tripInfo.parameters.end != undefined;
+                        if (isSafe) {
+                            actualParams.add("start");
+                            actualParams.add("end");
+                        }
+                        if (isSafe && tripInfo.parameters.limit != undefined) {
+                            if (tripInfo.parameters.interest.length
+                                    && !isNaN(tripInfo.parameters.limit = parseInt(tripInfo.parameters.limit)) && tripInfo.parameters.limit >= 0 && tripInfo.parameters.limit <= 8) {
+                                actualParams.add("limit");
+                            } else {
+                                delete tripInfo.parameters.limit;
+                            }
+                        }
+                        if (isSafe && tripInfo.parameters.minDistance != undefined) {
+                            if (actualParams.has("limit")
+                                    && !isNaN(tripInfo.parameters.minDistance = parseFloat(tripInfo.parameters.minDistance)) != NaN
+                                    && tripInfo.parameters.minDistance >= 0) {
+                                actualParams.add("minDistance");
+                            } else {
+                                delete tripInfo.parameters.minDistance;
+                            }
+                        }
+                        if (isSafe && tripInfo.parameters.maxDetour != undefined) {
+                            if (actualParams.has("minDistance")
+                                    && !isNaN(tripInfo.parameters.maxDetour = parseFloat(tripInfo.parameters.maxDetour)) != NaN
+                                    && tripInfo.parameters.maxDetour >= 0 && tripInfo.parameters.maxDetour <= tripInfo.parameters.minDistance/2) {
+                                actualParams.add("maxDetour");
+                            } else {
+                                delete tripInfo.parameters.maxDetour;
+                            }
+                        }
+                        if (isSafe && tripInfo.parameters.profile != undefined) {
+                            if (actualParams.has("limit")
+                                    && PROFILES[tripInfo.parameters.profile] != undefined) {
+                                actualParams.add("profile");
+                            } else {
+                                delete tripInfo.parameters.profile;
+                            }
+                        }
+                        break;
+                    default:
+                        isSafe = false;
+                        break;
+                }
+            }  
+
+            if (isSafe && isCorrect(Object.keys(tripInfo.parameters),actualParams)) {
+
+                tripInfo.dateTime = new Date().toISOString();
+
+                user.history.push(tripInfo);
+                user.save((err, updatedUser) => {
+                    if(err) {
+                        //console.log(err);
+                        res.status(500).json({
+                            error: MSG.serverError
+                        });
+                    } else {
+                        res.status(200).json(updatedUser.history[updatedUser.history.length-1]);
+                    }
+                });
+            } else {
+                res.status(500).json({
+                    error: MSG.serverError
+                });
             }
         }
     });
@@ -91,7 +148,7 @@ module.exports.getTrips = async (req, res) => {
     const strOffset = req.query.offset;
 
     let history = {};
-    let mergedHistory;
+    let mergedHistory,strAllParams,param,tmpTrip;
     User.findById(req.loggedUser.user_id,{_id: 0, history: 1},(err,docs) => {
         if (err) {
             //console.log(err);
@@ -106,10 +163,21 @@ module.exports.getTrips = async (req, res) => {
                 return dateA < dateB ? 1 : (dateA > dateB ? -1 : 0);
             });
             for (const trip of mergedHistory) {
-                if (history[trip.type] == undefined) {
-                    history[trip.type] = [trip];
+                tmpTrip = trip.toObject();
+                strAllParams = "";
+                for (const key of Object.keys(tmpTrip.parameters)) {
+                    param = tmpTrip.parameters[key];
+                    if (Array.isArray(param)) {
+                        strAllParams += arrayToStr(param,"&" + key + "=",true);
+                    } else if (param != undefined) {
+                        strAllParams += "&" + key + "=" + param;
+                    }
+                }
+                tmpTrip.url = tmpTrip.route + "/" + tmpTrip.type + "?" + strAllParams;
+                if (history[tmpTrip.type] == undefined) {
+                    history[tmpTrip.type] = [tmpTrip];
                 } else {
-                    history[trip.type].push(trip);
+                    history[tmpTrip.type].push(tmpTrip);
                 }
             }
             for (const type of Object.keys(history)) {
@@ -133,7 +201,7 @@ module.exports.deleteTrip = async (req, res) => {
 
     User.findByIdAndUpdate(req.loggedUser.user_id,{
         $pull: {
-            history: {id: id},
+            history: {_id: id},
         }
     },(err,doc) => {
         if (err) {
@@ -158,4 +226,19 @@ function cut(array,strOffset,strLimit) {
     }
 
     return array;
+}
+
+function isCorrect(toCheck,checker) {
+
+    let right = true;
+    let i = 0;
+    if (toCheck.length == checker.size) {
+        do {
+            right = checker.has(toCheck[i]);
+        } while (right && ++i < toCheck.length);
+    } else {
+        right = false;
+    }
+
+    return right;
 }
