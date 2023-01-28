1d436daac0c974b5670d4a5b33fe87decbad0733
Simone <simone.compri@gmail.com>
2023-01-28 02:45:37 +0100

    Improved geocode accuracy
    Possibility of:
    - The use of a referring point to find the closest result
    - Ask that the closest result be a place (e.g. city)

--- processes/OSMTools/controllers/routingAdapter.js
+++ processes/OSMTools/controllers/routingAdapter.js
@@ -1,11 +1,14 @@
 'use strict';
 
-const { fetch, strToPoint } = require("../../../common/functions");
+const { fetch, strToPoint, pointObjCompleteNames, pointArrayToObj, parseBoolean } = require("../../../common/functions");
 const { PROFILES } = require("../../../common/dataStructures");
+
+const geolib = require('geolib');
 
 let MSG = {
     badRequest: "Bad Request", //Error code: 400
     serverError: "Server error", //Error code: 500
+    notFound: "Data not found", //Error code: 404
 }
 
 module.exports.geocode = async (req, res) => {
@@ -17,8 +20,23 @@ module.exports.geocode = async (req, res) => {
 
     let address = req.query.address; //Controlli su Injection
     let limit = req.query.limit;
+    let closestToStr = req.query.closestTo;
+    let onlyPlacesStr = req.query.onlyPlaces;
 
-    if (address != undefined && (limit == undefined || !isNaN(limit))) {
+    let onlyPlaces = false;
+    let closest = {
+        details: undefined,
+        dist: Number.MAX_SAFE_INTEGER
+    };
+    let closestPlace = {
+        details: undefined,
+        dist: Number.MAX_SAFE_INTEGER
+    };
+    let closestTo,currentDist;
+    if (address != undefined
+            && (limit == undefined || !isNaN(limit))
+            && (closestToStr == undefined || (closestTo = pointArrayToObj(strToPoint(closestToStr))) != null)
+            && (onlyPlacesStr == undefined || closestTo != undefined && (onlyPlaces = parseBoolean(onlyPlacesStr)) != null)) {
         fetch("https://graphhopper.com/api/1/geocode"
         + "?key=" + process.env.GRAPHHOPPER_KEY
         + "&q=" + address
@@ -27,7 +45,32 @@ module.exports.geocode = async (req, res) => {
             headers: {
             'Accept': 'application/json'
             }
-        }).then(response => response.json()).then(response => res.status(200).json(response))
+        }).then(response => response.json()).then(response => {
+            if (Array.isArray(response.hits) && response.hits.length > 0) {
+                if (closestTo != undefined) {
+                    for (const result of response.hits) {
+                        currentDist = geolib.getDistance(closestTo,pointObjCompleteNames(result.point));
+                        if (currentDist < closest.dist) {
+                            closest.dist = currentDist;
+                            closest.details = result;
+                        }
+                        if (onlyPlaces && result.osm_key == "place" && currentDist < closestPlace.dist) {
+                            closestPlace.dist = currentDist;
+                            closestPlace.details = result;
+                        }
+                    }
+                    res.status(200).json({
+                        hits: [onlyPlaces && closestPlace.details != undefined ? closestPlace.details : closest.details]
+                    });
+                } else {
+                    res.status(200).json(response);
+                }
+            } else {
+                res.status(404).json({
+                    error: MSG.notFound
+                });
+            }
+        })
         .catch(err => {
             //console.error(err);
             res.status(400).json({
@@ -54,7 +97,7 @@ module.exports.route = async (req, res) => {
     
     let right = true;
     let points;
-    let i = 0, j;
+    let i = 0;
     if (strPoints != undefined && Array.isArray(strPoints) && strPoints.length >= 2
      && (profile == undefined || PROFILES[profile] != undefined)
      && (shortAnswer == undefined || shortAnswer === 'true' || shortAnswer === 'false')) {
